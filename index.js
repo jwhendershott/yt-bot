61170dc75a185a006a9193cbaa6237a704005232
jwhendershott <jwhendershott@gmail.com>
2023-07-30 23:19:33 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv/config');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+client.on('ready', () => {
+    console.log('The bot is ready');
+});
+
+client.on('messageCreate', async message => {
+    if (message.content === 'ping') {
+        message.reply('pong');
+    }
+
+    if (message.content === '!youtube') {
+        message.channel.send('Youtube!');
+
+        let url = `GET https://www.googleapis.com/youtube/v3/search`
+        let response = await fetch(url);
+        let json = await response.json();
+        console.log(json);
+    }
+});
+
+client.login(process.env.TOKEN);
