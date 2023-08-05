de7794f695df78e220ed763718320470c1e8399a
jwhendershott <jwhendershott@gmail.com>
2023-08-04 19:43:43 -0500

    Updated API call and Embed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, EmbedBuilder} = require('discord.js');
 require('dotenv/config');
 
 const client = new Client({
@@ -15,15 +15,25 @@ client.on('ready', () => {
 
 client.on('messageCreate', async message => {
     if (message.content === 'ping') {
-        message.reply('pong');
+        await message.reply('pong');
     }
 
-    if (message.content === '!youtube') {
-        message.channel.send('Youtube!');
-        let url = `https://www.googleapis.com/youtube/v3/search?key=${KEY}a=${searchText}&type=video`
+    if (message.content.toLowerCase().includes('!youtube')) {
+        await message.channel.send('Youtube!');
+        let filter = m => m.author.id === message.author.id;
+        let collector = message.channel.createMessageCollector(filter, { max: 1}); //not set up yet
+        let embed = new EmbedBuilder()
+            .setColor("#73ffdc")
+            .setDescription("Query:")
+            .setTitle("YT-BOT Search")
+        // let embedMsg = await message.channel.send(embed);
+        let searchText = message.content.split(' ')[1];
+        console.log(process.env.KEY);
+        let url = `https://www.googleapis.com/youtube/v3/search?key=${process.env.KEY}&q=${searchText}&type=video`
         let response = await fetch(url);
         let json = await response.json();
-        console.log(json);
+        console.log(json.items[0].id.videoId);
+        let embedMsg = await message.channel.send(`https://www.youtube.com/watch?v=${json.items[0].id.videoId}`);
     }
 });
 
